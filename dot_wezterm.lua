b09a02928ecb673aa3af574f0eb090cdd2654d24
Connor Hanna <connordouglashanna@gmail.com>
2026-03-21 00:05:12 -0500

    changed the color of the directory text to lavender, and added fzf to the .zshrc

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,13 +5,23 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
+-- setting tmux-style "leader"
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1500 }
+
+-- tmux split shortcuts 
+config.keys = {
+  -- Split horizontal (right) with 'Ctrl-B' then '%'
+  { key = '%', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+   -- Split vertical (down) with 'Ctrl-B' then '"'
+ { key = '"', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+}
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 34
 
 -- or, changing the font size and color scheme.
-config.font_size = 11
+config.font_size = 15
 
 -- Ricing "Usual Suspects"
 config.window_padding = { left = 20, right = 20, top = 20, bottom = 20 }
